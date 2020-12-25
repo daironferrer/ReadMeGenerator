40d0957e438d84c22b2240368ceaa12194214dcb
unknown <daironferrer7@gmail.com>
2020-12-24 20:06:42 -0500

    added files

---
+++ index.js
@@ -0,0 +1,22 @@
+// array of questions for user
+const questions = [
+    "What is the name of the project?",
+    "Describe your project",
+    "What is the installation process?",
+    "License?",
+    "Any other contributors?",
+    "Github Username: ",
+    "Email: "
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
