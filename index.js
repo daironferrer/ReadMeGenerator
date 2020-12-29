baba88604c09c6c1f5d2b5332fadc06120a9de76
unknown <daironferrer7@gmail.com>
2020-12-28 22:15:54 -0500

    added functions and array questions

--- index.js
+++ index.js
@@ -1,21 +1,66 @@
+const inquirer = require("inquirer");
+const fs = require('fs');
+const generateMarkdown = require('generateMarkdown');
+
 // array of questions for user
 const questions = [
-    "What is the name of the project?",
-    "Describe your project",
-    "What is the installation process?",
-    "License?",
-    "Any other contributors?",
-    "Github Username: ",
-    "Email: "
+{
+    type:'input',
+    message:'What is the title of your project?',
+    name:'projectTitle'
+},
+
+{
+    type:'input',
+    message:'Write a short description of your project',
+    name:'projectDes'
+},
+
+{
+    type:'input',
+    message:'Does anything need to be installed?',
+    name:'projectInstall'
+},
+
+{
+    type:'input',
+    message:'What type of license?',
+    name:'projectLicense'
+},
+
+{
+    type:'input',
+    message:'Can anyone help with this project?',
+    name:'projectCollab'
+},
+
+{
+    type:'input',
+    message:'What is your GitHub?',
+    name:'projectGit'
+},
+
+{
+    type:'input',
+    message:'What is your email?',
+    name:'projectEmail'
+},
+
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    let file = generateMarkdown(data)
+    console.log(fileName, "/n", file)
+    fs.writeFileSync(fileName, file);
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions).then(response => {
+        let fileName = response.title.split('').join('') + '.md'
+        writeToFile(fileName, response)
+    })
 }
 
 // function call to initialize program
